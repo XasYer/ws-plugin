633e05ebbce7185bb850e84e49f2328c1d55298b
小叶 <1936472877@qq.com>
2023-05-28 19:41:29 +0800

    增加帮助,仅主人可用

--- apps/help.js
+++ apps/help.js
@@ -1,5 +1,8 @@
 import plugin from '../../../lib/plugins/plugin.js'
 import { Render, Version } from '../components/index.js'
+import { helpCfg, helpList } from '../config/system/help_system.js'
+import { style } from '../resources/help/imgs/config.js'
+import _ from 'lodash'
 
 export class setting extends plugin {
     constructor() {
@@ -12,6 +15,11 @@ export class setting extends plugin {
                 {
                     reg: '^#ws版本$',
                     fnc: 'version'
+                },
+                {
+                    reg: '^#ws帮助$',
+                    fnc: 'help',
+                    permission: 'master'
                 }
             ]
         })
@@ -25,4 +33,80 @@ export class setting extends plugin {
         }, { e, scale: 1.2 })
     }
 
+    async help(e) {
+        let helpGroup = []
+        _.forEach(helpList, (group) => {
+            _.forEach(group.list, (help) => {
+                let icon = help.icon * 1
+                if (!icon) {
+                    help.css = 'display:none'
+                } else {
+                    let x = (icon - 1) % 10
+                    let y = (icon - x - 1) / 10
+                    help.css = `background-position:-${x * 50}px -${y * 50}px`
+                }
+            })
+
+            helpGroup.push(group)
+        })
+
+        let themeData = await getThemeData(helpCfg, helpCfg)
+        return await Render.render('help/index', {
+            helpCfg,
+            helpGroup,
+            ...themeData,
+            element: 'default'
+        }, { e, scale: 1.6 })
+    }
+
+}
+
+async function getThemeCfg() {
+    let resPath = '{{_res_path}}/help/imgs/'
+    return {
+        main: `${resPath}/main.png`,
+        bg: `${resPath}/bg.jpg`,
+        style: style
+    }
+}
+
+async function getThemeData(diyStyle, sysStyle) {
+    let helpConfig = _.extend({}, sysStyle, diyStyle)
+    let colCount = Math.min(5, Math.max(parseInt(helpConfig?.colCount) || 3, 2))
+    let colWidth = Math.min(500, Math.max(100, parseInt(helpConfig?.colWidth) || 265))
+    let width = Math.min(2500, Math.max(800, colCount * colWidth + 30))
+    let theme = await getThemeCfg()
+    let themeStyle = theme.style || {}
+    let ret = [`
+    body{background-image:url(${theme.bg});width:${width}px;}
+    .container{background-image:url(${theme.main});width:${width}px;}
+    .help-table .td,.help-table .th{width:${100 / colCount}%}
+    `]
+    let css = function (sel, css, key, def, fn) {
+        let val = getDef(themeStyle[key], diyStyle[key], sysStyle[key], def)
+        if (fn) {
+            val = fn(val)
+        }
+        ret.push(`${sel}{${css}:${val}}`)
+    }
+    css('.help-title,.help-group', 'color', 'fontColor', '#ceb78b')
+    css('.help-title,.help-group', 'text-shadow', 'fontShadow', 'none')
+    css('.help-desc', 'color', 'descColor', '#eee')
+    css('.cont-box', 'background', 'contBgColor', 'rgba(43, 52, 61, 0.8)')
+    css('.cont-box', 'backdrop-filter', 'contBgBlur', 3, (n) => diyStyle.bgBlur === false ? 'none' : `blur(${n}px)`)
+    css('.help-group', 'background', 'headerBgColor', 'rgba(34, 41, 51, .4)')
+    css('.help-table .tr:nth-child(odd)', 'background', 'rowBgColor1', 'rgba(34, 41, 51, .2)')
+    css('.help-table .tr:nth-child(even)', 'background', 'rowBgColor2', 'rgba(34, 41, 51, .4)')
+    return {
+        style: `<style>${ret.join('\n')}</style>`,
+        colCount
+    }
+}
+
+function getDef() {
+    for (let idx in arguments) {
+        if (!_.isUndefined(arguments[idx])) {
+            return arguments[idx]
+        }
+    }
 }
---
+++ config/system/help_system.js
@@ -0,0 +1,64 @@
+export const helpCfg = {
+  "themeSet": false,
+  "title": "ws帮助",
+  "subTitle": "Yunzai-Bot & ws-plugin",
+  "colWidth": 265,
+  "theme": "all",
+  "themeExclude": [
+    "default"
+  ],
+  "colCount": 2,
+  "bgBlur": true
+}
+export const helpList = [
+  {
+    "group": "连接管理",
+    "list": [
+      {
+        "icon": 80,
+        "title": "#ws添加连接",
+        "desc": "添加一个新的连接"
+      },
+      {
+        "icon": 63,
+        "title": "#ws删除连接",
+        "desc": "删除一个已有的连接 "
+      },
+      {
+        "icon": 66,
+        "title": "#ws关闭连接",
+        "desc": "不会删除已有连接,同时不进行连接"
+      },
+      {
+        "icon": 65,
+        "title": "#ws打开连接",
+        "desc": "打开已关闭的连接"
+      },
+      {
+        "icon": 79,
+        "title": "#ws查看连接",
+        "desc": "查看已有的所有连接名字和状态"
+      },
+      {
+        "icon": 64,
+        "title": "#ws重新连接",
+        "desc": "强制断开已有的所有连接并重新连接"
+      }
+    ]
+  },
+  {
+    "group": "其他说明",
+    "list": [
+      {
+        "icon": 71,
+        "title": "#ws连接说明",
+        "desc": "查看添加连接时的说明"
+      },
+      {
+        "icon": 94,
+        "title": "#ws设置",
+        "desc": "插件设置"
+      }
+    ]
+  }
+]
