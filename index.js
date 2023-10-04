dc49284e51d810e5242d92e8bb78ad06072d193b
小叶 <1936472877@qq.com>
2023-10-04 19:24:46 +0800

    改下位置

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import fs from 'node:fs'
 import { initWebSocket, Config, Version } from './components/index.js'
-
+import { TMP_DIR, mimeTypes } from './model/index.js'
+import { join, extname } from 'path'
 const files = fs.readdirSync('./plugins/ws-plugin/apps').filter(file => file.endsWith('.js'))
 
 let ret = []
@@ -48,14 +49,31 @@ for (const item of path) {
     }
 }
 
-// if (!Version.isTrss) {
 initWebSocket()
-// } else {
-// Bot.on('connect', data => {
-//     if (data.version?.id == 'QQGuild') {
-//         initWebSocket(Config.servers, data.appID, data.uin)
-//     }
-// })
-// }
+if (Version.isTrss) {
+    Bot.express.get('/ws-plugin*', async (req, res) => {
+        const file = req.query.file
+        if (file) {
+            const ext = extname(file)
+            const contentType = mimeTypes[ext]
+            fs.readFile(join(TMP_DIR, file), (err, content) => {
+                if (err) {
+                    res.writeHead(404)
+                    res.end('File not found')
+                } else {
+                    const name = file.split('-')
+                    res.writeHead(200, {
+                        'Content-Type': contentType,
+                        'Content-Disposition': `attachment; filename=${name[1] || name[0]}`
+                    })
+                    res.end(content)
+                }
+            })
+            return
+        }
+        res.writeHead(404);
+        res.end('Page not found')
+    })
+}
 
 export { apps }
