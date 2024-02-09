bdd80a32265b467512b3f7952987453c4ccf08af
Rrrrrrray <7407060+Rrrrrrray@user.noreply.gitee.com>
2024-02-09 11:44:13 +0800

    ✨ feat: toImgNoTitle

--- components/Config.js
+++ components/Config.js
@@ -12,7 +12,7 @@ const Path = process.cwd()
 const Plugin_Name = 'ws-plugin'
 const Plugin_Path = `${Path}/plugins/${Plugin_Name}`
 class Config {
-  constructor() {
+  constructor () {
     this.config = {}
     this.oldConfig = {}
     /** 监听文件 */
@@ -22,7 +22,7 @@ class Config {
   }
 
   /** 初始化配置 */
-  initCfg() {
+  initCfg () {
     let path = `${Plugin_Path}/config/config/`
     if (!fs.existsSync(path)) fs.mkdirSync(path)
     let pathDef = `${Plugin_Path}/config/default_config/`
@@ -36,196 +36,201 @@ class Config {
   }
 
   /** 获得yunzai Bot.yaml配置 */
-  get bot() {
+  get bot () {
     return cfg.bot
   }
 
   /** 主人QQ */
-  get masterQQ() {
+  get masterQQ () {
     return cfg.masterQQ
   }
 
   /** Bot账号:[主人帐号] */
-  get master() {
+  get master () {
     return cfg.master
   }
 
   /** 云崽黑名单群 */
-  get blackGroup() {
+  get blackGroup () {
     return cfg.getOther().blackGroup
   }
 
   /** 云崽白名单群 */
-  get whiteGroup() {
+  get whiteGroup () {
     return cfg.getOther().whiteGroup
   }
 
   /** 云崽黑名单QQ */
-  get blackQQ() {
+  get blackQQ () {
     return cfg.getOther().blackQQ
   }
 
   /** 心跳 */
-  get heartbeatInterval() {
+  get heartbeatInterval () {
     return this.getDefOrConfig('ws-config').heartbeatInterval
   }
 
   /** 数据上报类型 */
-  get messagePostFormat() {
+  get messagePostFormat () {
     return this.getDefOrConfig('ws-config').messagePostFormat
   }
 
   /** 端口 */
-  get wsPort() {
+  get wsPort () {
     return this.getDefOrConfig('ws-config').wsPort
   }
 
   /** 是否忽略云崽配置文件的仅艾特和前缀,即不需要艾特或前缀即可上报消息 */
-  get ignoreOnlyReplyAt() {
+  get ignoreOnlyReplyAt () {
     return this.getDefOrConfig('ws-config').ignoreOnlyReplyAt
   }
 
   /** 连接列表 */
-  get servers() {
+  get servers () {
     return this.getDefOrConfig('ws-config').servers
   }
 
-  get noMsgStart() {
+  get noMsgStart () {
     return this.getDefOrConfig('msg-config').noMsgStart
   }
 
-  get noMsgInclude() {
+  get noMsgInclude () {
     return this.getDefOrConfig('msg-config').noMsgInclude
   }
 
-  get howToMaster() {
+  get howToMaster () {
     return this.getDefOrConfig('msg-config').howToMaster
   }
 
-  /**掉线时否通知主人 */
-  get disconnectToMaster() {
+  /** 掉线时否通知主人 */
+  get disconnectToMaster () {
     return this.getDefOrConfig('msg-config').disconnectToMaster
   }
 
-  /**重连成功时是否通知主人 */
-  get reconnectToMaster() {
+  /** 重连成功时是否通知主人 */
+  get reconnectToMaster () {
     return this.getDefOrConfig('msg-config').reconnectToMaster
   }
 
-  /**首次连接成功时是否通知主人 */
-  get firstconnectToMaster() {
+  /** 首次连接成功时是否通知主人 */
+  get firstconnectToMaster () {
     return this.getDefOrConfig('msg-config').firstconnectToMaster
   }
 
-  /**消息存储时间 */
-  get msgStoreTime() {
+  /** 消息存储时间 */
+  get msgStoreTime () {
     return this.getDefOrConfig('msg-config').msgStoreTime
   }
 
-  /**禁用群聊列表 */
-  get noGroup() {
+  /** 禁用群聊列表 */
+  get noGroup () {
     return this.getDefOrConfig('msg-config').noGroup
   }
 
   /** 白名单群聊 */
-  get yesGroup() {
+  get yesGroup () {
     return this.getDefOrConfig('msg-config').yesGroup
   }
 
   /** 禁言拦截 */
-  get muteStop() {
+  get muteStop () {
     return this.getDefOrConfig('msg-config').muteStop
   }
 
   /** red 发送伪造转发消息方式 */
-  get redSendForwardMsgType() {
+  get redSendForwardMsgType () {
     return this.getDefOrConfig('msg-config').redSendForwardMsgType
   }
 
   /** 文字转图片是否展示ID */
-  get toImgID() {
+  get toImgID () {
     return this.getDefOrConfig('msg-config').toImgID
   }
 
+  /** 转图片是否不包含标题 */
+  get toImgNoTitle () {
+    return this.getDefOrConfig('msg-config').toImgNoTitle
+  }
+
   /** 渲染精度 */
-  get renderScale() {
+  get renderScale () {
     return this.getDefOrConfig('msg-config').renderScale
   }
 
   /** 数据库同步锁 */
-  get taskQueue() {
+  get taskQueue () {
     return this.getDefOrConfig('msg-config').taskQueue
   }
 
-  /**群管理员变动是否上报 */
-  get groupAdmin() {
+  /** 群管理员变动是否上报 */
+  get groupAdmin () {
     return this.getDefOrConfig('notice-config').groupAdmin
   }
 
-  /**群成员减少是否上报 */
-  get groupDecrease() {
+  /** 群成员减少是否上报 */
+  get groupDecrease () {
     return this.getDefOrConfig('notice-config').groupDecrease
   }
 
-  /**群成员增加是否上报 */
-  get groupIncrease() {
+  /** 群成员增加是否上报 */
+  get groupIncrease () {
     return this.getDefOrConfig('notice-config').groupIncrease
   }
 
-  /**群禁言是否上报 */
-  get groupBan() {
+  /** 群禁言是否上报 */
+  get groupBan () {
     return this.getDefOrConfig('notice-config').groupBan
   }
 
-  /**好友添加是否上报 */
-  get friendIncrease() {
+  /** 好友添加是否上报 */
+  get friendIncrease () {
     return this.getDefOrConfig('notice-config').friendIncrease
   }
 
-  /**群消息撤回是否上报 */
-  get groupRecall() {
+  /** 群消息撤回是否上报 */
+  get groupRecall () {
     return this.getDefOrConfig('notice-config').groupRecall
   }
 
-  /**好友消息撤回是否上报 */
-  get friendRecall() {
+  /** 好友消息撤回是否上报 */
+  get friendRecall () {
     return this.getDefOrConfig('notice-config').friendRecall
   }
 
-  /**群内戳一戳是否上报 */
-  get groupPoke() {
+  /** 群内戳一戳是否上报 */
+  get groupPoke () {
     return this.getDefOrConfig('notice-config').groupPoke
   }
 
   /** 好友申请是否上报 */
-  get friendAdd() {
+  get friendAdd () {
     return this.getDefOrConfig('request-config').friendAdd
   }
 
   /** 群聊邀请是否上报 (邀请机器人入群) */
-  get groupInvite() {
+  get groupInvite () {
     return this.getDefOrConfig('request-config').groupInvite
   }
 
   /** 群聊申请是否上报 (申请加入群聊) */
-  get groupAdd() {
+  get groupAdd () {
     return this.getDefOrConfig('request-config').groupAdd
   }
 
   /** 默认配置和用户配置 */
-  getDefOrConfig(name) {
+  getDefOrConfig (name) {
     let def = this.getdefSet(name)
     let config = this.getConfig(name)
     return { ...def, ...config }
   }
 
   /** 默认配置 */
-  getdefSet(name) {
+  getdefSet (name) {
     return this.getYaml('default_config', name)
   }
 
   /** 用户配置 */
-  getConfig(name) {
+  getConfig (name) {
     return this.getYaml('config', name)
   }
 
@@ -234,7 +239,7 @@ class Config {
    * @param type 默认跑配置-defSet，用户配置-config
    * @param name 名称
    */
-  getYaml(type, name) {
+  getYaml (type, name) {
     let file = `${Plugin_Path}/config/${type}/${name}.yaml`
     let key = `${type}.${name}`
 
@@ -250,7 +255,7 @@ class Config {
   }
 
   /** 监听配置文件 */
-  watch(file, name, type = 'default_config') {
+  watch (file, name, type = 'default_config') {
     let key = `${type}.${name}`
     if (!this.oldConfig[key]) this.oldConfig[key] = _.cloneDeep(this.config[key])
     if (this.watcher[key]) return
@@ -269,7 +274,7 @@ class Config {
         // console.log(object);
         for (const key in object) {
           if (Object.hasOwnProperty.call(object, key)) {
-            const value = object[key];
+            const value = object[key]
             const arr = key.split('.')
             if (arr[0] !== 'servers') continue
             let data = newConfig.servers[arr[1]]
@@ -278,29 +283,25 @@ class Config {
               type: null,
               data
             }
-            if (typeof value['newValue'] === 'object' && typeof value['oldValue'] === 'undefined') {
+            if (typeof value.newValue === 'object' && typeof value.oldValue === 'undefined') {
               target.type = 'add'
-            }
-            else if (typeof value['newValue'] === 'undefined' && typeof value['oldValue'] === 'object') {
+            } else if (typeof value.newValue === 'undefined' && typeof value.oldValue === 'object') {
               target.type = 'del'
-            }
-            else if (value['newValue'] === true && (value['oldValue'] === false || typeof value['oldValue'] === 'undefined')) {
+            } else if (value.newValue === true && (value.oldValue === false || typeof value.oldValue === 'undefined')) {
               target.type = 'close'
-            }
-            else if (value['newValue'] === false && (value['oldValue'] === true || typeof value['oldValue'] === 'undefined')) {
+            } else if (value.newValue === false && (value.oldValue === true || typeof value.oldValue === 'undefined')) {
               target.type = 'open'
             }
             await modifyWebSocket(target)
           }
         }
-
       }
     })
 
     this.watcher[key] = watcher
   }
 
-  getCfgSchemaMap() {
+  getCfgSchemaMap () {
     let ret = {}
     _.forEach(cfgSchema, (cfgGroup) => {
       _.forEach(cfgGroup.cfg, (cfgItem, cfgKey) => {
@@ -311,11 +312,11 @@ class Config {
     return ret
   }
 
-  getCfgSchema() {
+  getCfgSchema () {
     return cfgSchema
   }
 
-  getCfg() {
+  getCfg () {
     let wsconfig = this.getDefOrConfig('ws-config')
     let msgconfig = this.getDefOrConfig('msg-config')
     let noticeconfig = this.getDefOrConfig('notice-config')
@@ -335,7 +336,7 @@ class Config {
    * @param {String|Number} value 修改的value值
    * @param {'config'|'default_config'} type 配置文件或默认
    */
-  modify(name, key, value, type = 'config') {
+  modify (name, key, value, type = 'config') {
     let path = `${Plugin_Path}/config/${type}/${name}.yaml`
     new YamlReader(path).set(key, value)
     this.oldConfig[key] = _.cloneDeep(this.config[key])
@@ -350,7 +351,7 @@ class Config {
    * @param {'add'|'del'} category 类别 add or del
    * @param {'config'|'default_config'} type 配置文件或默认
    */
-  modifyarr(name, key, value, category = 'add', type = 'config') {
+  modifyarr (name, key, value, category = 'add', type = 'config') {
     let path = `${Plugin_Path}/config/${type}/${name}.yaml`
     let yaml = new YamlReader(path)
     if (category == 'add') {
@@ -361,20 +362,20 @@ class Config {
     }
   }
 
-  setArr(name, key, item, value, type = 'config') {
+  setArr (name, key, item, value, type = 'config') {
     let path = `${Plugin_Path}/config/${type}/${name}.yaml`
     let yaml = new YamlReader(path)
-    let arr = yaml.get(key).slice();
+    let arr = yaml.get(key).slice()
     arr[item] = value
     yaml.set(key, arr)
   }
 
-  delServersArr(value, name = 'ws-config', type = 'config') {
+  delServersArr (value, name = 'ws-config', type = 'config') {
     let path = `${Plugin_Path}/config/${type}/${name}.yaml`
     let yaml = new YamlReader(path)
     let key = 'servers'
     // let index = yaml.jsonData[key].indexOf(value)
-    let index = yaml.jsonData[key].findIndex(item => item.name === value);
+    let index = yaml.jsonData[key].findIndex(item => item.name === value)
     yaml.delete(`${key}.${index}`)
   }
 
@@ -385,26 +386,26 @@ class Config {
    * @param {*} parentKey 
    * @returns 
    */
-  findDifference(obj1, obj2, parentKey = '') {
-    const result = {};
+  findDifference (obj1, obj2, parentKey = '') {
+    const result = {}
     for (const key in obj1) {
-      const fullKey = parentKey ? `${parentKey}.${key}` : key;
+      const fullKey = parentKey ? `${parentKey}.${key}` : key
       if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-        const diff = this.findDifference(obj1[key], obj2[key], fullKey);
+        const diff = this.findDifference(obj1[key], obj2[key], fullKey)
         if (!_.isEmpty(diff)) {
-          Object.assign(result, diff);
+          Object.assign(result, diff)
         }
       } else if (!_.isEqual(obj1[key], obj2[key])) {
-        result[fullKey] = { oldValue: obj1[key], newValue: obj2[key] };
+        result[fullKey] = { oldValue: obj1[key], newValue: obj2[key] }
       }
     }
     for (const key in obj2) {
       if (!obj1.hasOwnProperty(key)) {
-        const fullKey = parentKey ? `${parentKey}.${key}` : key;
-        result[fullKey] = { oldValue: undefined, newValue: obj2[key] };
+        const fullKey = parentKey ? `${parentKey}.${key}` : key
+        result[fullKey] = { oldValue: undefined, newValue: obj2[key] }
       }
     }
-    return result;
+    return result
   }
 }
 export default new Config()
