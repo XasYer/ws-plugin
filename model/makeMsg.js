92c2b43d8aa6491fbbc184a4a292a353b86f5f1f
小叶 <1936472877@qq.com>
2024-03-05 21:58:27 +0800

    优化支持Lain

--- model/makeMsg.js
+++ model/makeMsg.js
@@ -149,7 +149,7 @@ async function makeGSUidReportMsg(e) {
  */
 async function makeGSUidSendMsg(data) {
     let content = data.content, quote = null, bot = Bot[data.bot_self_id] || Bot
-	const sendMsg = []
+    const sendMsg = []
     if (content[0].type.startsWith('log')) {
         logger.info(content[0].data);
     } else {
@@ -189,17 +189,16 @@ async function makeGSUidSendMsg(data) {
                     sendMsg.push(await bot[target](data.target_id).makeForwardMsg?.(arr) || { type: 'node', data: arr })
                     break;
                 case 'template_markdown':
-					const markdown_parms = [];
-					const parms_keys = Object.keys(msg.data.para);
-					for (const keys of parms_keys) {
-						markdown_parms.push({key:keys,values:[msg.data.para[keys]]});
-					}
-					sendMsg.push({ type: 'markdown', data: {custom_template_id:msg.data.template_id,params:markdown_parms} });
-					break;
+                    const markdown_parms = [];
+                    for (const key in msg.data.para) {
+                        markdown_parms.push({ key, values: [msg.data.para[key]] });
+                    }
+                    const data = { custom_template_id: msg.data.template_id, params: markdown_parms }
+                    sendMsg.push(toMD(data));
+                    break;
                 case 'buttons':
-                    const buttons_data=await toButton(msg);
-					sendMsg.push({ type: 'button', data: buttons_data });
-					break;
+                    sendMsg.push(toButton(msg));
+                    break;
                 default:
                     break;
             }
@@ -208,49 +207,62 @@ async function makeGSUidSendMsg(data) {
     return { sendMsg, quote }
 }
 
-
-async function toButton(obj) {
-    const button = [];
-    let arr=[];
-    const sum = 2;//每行几个
-    for (let j of obj.data) {
-		let action = j.action;
-		if(action == -1){
-			action = 2;
-		}
-		let enter = false
-		if(action == 1){
-			action = 2;
-			enter = true;
-		}
-		let specify_user_ids = j.specify_user_ids;
-		if(j.specify_user_ids.length == 0){
-			specify_user_ids = false;
-		}
-		let input_data = j.data
-		if(j.data == ''){
-			input_data = '/';
-		}
-		arr.push({
-			"text": j.text,
-			"clicked_text": j.text,
-			"input": input_data,
-			"send": enter,
-			"action": action,
-			"permission": specify_user_ids
-		},);
-
-		if ( arr.length>=sum) {
-			button.push( arr);
-			arr = [];
-		}else if (arr.length>=obj.data.length){
-			button.push( arr);
-			arr = [];
-		}
-
+function toMD(data) {
+    if (Version.isTrss) {
+        return segment.markdown(data)
+    } else {
+        return {
+            type: 'markdown',
+            ...data
+        }
     }
-    return button
-  }
+}
+
+function toButton(obj) {
+    const buttons = [];
+    let button = [];
+    const sum = 2; // 每行几个
+    for (let j of obj.data) {
+        let action = j.action;
+        if (action == -1) {
+            action = 2;
+        }
+        let enter = false
+        if (action == 1) {
+            action = 2;
+            enter = true;
+        }
+        let specify_user_ids = j.specify_user_ids;
+        if (j.specify_user_ids.length == 0) {
+            specify_user_ids = false;
+        }
+        let input_data = j.data
+        if (j.data == '') {
+            input_data = '/';
+        }
+        button.push({
+            text: j.text,
+            clicked_text: j.text,
+            input: input_data,
+            send: enter,
+            // action: action,
+            permission: specify_user_ids
+        },);
+
+        if (button.length >= sum) {
+            buttons.push(button);
+            button = [];
+        } else if (button.length >= obj.data.length) {
+            buttons.push(button);
+            button = [];
+        }
+    }
+    if (Version.isTrss) {
+        return segment.button(...buttons)
+    } else {
+        return Bot.Button(buttons)
+    }
+}
 
 /**
  * 制作onebot发送的消息
